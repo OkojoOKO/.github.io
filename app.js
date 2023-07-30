1376d69a929aa52fadc9b5741857e7a6c2f55c7f
OkojoOKO <140716729+OkojoOKO@users.noreply.github.com>
2023-07-30 21:13:01 +0900

    Update app.js

--- app.js
+++ app.js
@@ -22,12 +22,17 @@ document.getElementById('fileInput').addEventListener('change', function(event) 
 });
 
 function parseCSV(data) {
-  // CSVデータを解析してxとyのデータを抽出する処理
-  // 例えば、以下のような形式を想定
-  // x,y
-  // 1,5
-  // 2,10
-  // ...
-  // 解析した結果を返す
-}
+  var lines = data.split('\n');
+  var x = [];
+  var y = [];
 
+  for (var i = 1; i < lines.length; i++) {
+    var values = lines[i].split(',');
+    if (values.length >= 2) {
+      x.push(parseFloat(values[0]));
+      y.push(parseFloat(values[1]));
+    }
+  }
+
+  return { x: x, y: y };
+}
